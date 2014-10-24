d7abe69bf152e7029df4ec98521cae14e5807c5c
José Manuel Sánchez <buscarini@gmail.com>
2014-10-24 15:49:08 +0200

    Added some extensions for string. Added condition and validation and more tests

---
+++ pairofwings/pairofwings/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  pairofwings
+//
+//  Created by Jose Manuel Sánchez Peñarroja on 24/10/14.
+//  Copyright (c) 2014 José Manuel Sánchez. All rights reserved.
+//
+
+import Foundation
---
+++ pairofwings/pairofwingsTests/ConditionTests.swift
@@ -0,0 +1,9 @@
+//
+//  ConditionTests.swift
+//  pairofwings
+//
+//  Created by Jose Manuel Sánchez Peñarroja on 24/10/14.
+//  Copyright (c) 2014 José Manuel Sánchez. All rights reserved.
+//
+
+import Foundation
